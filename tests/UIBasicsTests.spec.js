81c39b467cf6ebf8cc84c1883b5a6cd8703e6f2e
Marininha <m.framos@hotmail.com>
2026-04-23 21:04:11 -0300

    First Commit
    All files - first commit

---
+++ tests/UIBasicsTests.spec.js
@@ -0,0 +1,43 @@
+const {test, expect} = require('@playwright/test');
+
+
+test('Browser Context Playwright test',async ({browser})=>
+{
+      const context = await browser.newContext(); 
+      const page = await context.newPage();
+      await page.goto("https://the-internet.herokuapp.com/")
+
+})
+
+test('Page Playwright test',async ({page})=>
+{
+    await page.goto("https://the-internet.herokuapp.com/");
+    console.log(await page.title());
+    await expect(page).toHaveTitle("The Internet");
+
+})
+
+test('Page Playwright Login-Success',async ({page})=>
+{
+    await page.goto("https://practice.expandtesting.com/login");
+    console.log(await page.title());
+    //await page.locator('#username').type("practice")  -old
+    await page.locator('#username').fill("practice");
+    await page.locator('#password').fill("SuperSecretPassword!");
+    await page.locator('#submit-login').click();
+    await expect(page).toHaveTitle("Secure Page page for Automation Testing Practice");
+
+})
+
+test.only('Page Playwright Login-Fail',async ({page})=>
+{
+    await page.goto("https://practice.expandtesting.com/login");
+    console.log(await page.title());
+    //await page.locator('#username').type("practice")  -old
+    await page.locator('#username').fill("practice123");
+    await page.locator('#password').fill("SuperSecretPassword!");
+    await page.locator('#submit-login').click();
+    await expect(page.locator('#flash')).toHaveText('Your password is invalid!');
+
+})
+
